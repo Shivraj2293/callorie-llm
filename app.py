5886a192aaf91f2781a58dd498f3a1b1f6add093
ss2262 <79353985+ss2262@users.noreply.github.com>
2024-09-26 20:32:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+# Q&A Chatbot
+#from langchain.llms import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+from PIL import Image
+
+
+import google.generativeai as genai
+
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(input,image,prompt):
+    model = genai.GenerativeModel("gemini-1.5-pro")
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+input_prompt = """
+              You are a nutrition expert. Analyze the food items shown in the image and provide a detailed breakdown of their nutritional content. For each food item, list the following information in short:
+
+1. Item Name:
+   - Calories: [Number of calories]
+   - Protein: [Grams of protein]
+   - Carbohydrates: [Grams of carbohydrates]
+   - Fats: [Grams of fats]
+   - Fiber: [Grams of fiber]
+   - Sugars: [Grams of sugars]
+---
+Total Calories: [Sum of all calories]
+dont answer i cannot give you exact details give estimated information
+suggest dietary recommendations according to indian cusine in short
+
+               """
+
+## If ask button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
